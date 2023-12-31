6cd609fd7e186af8a6f338ad417eaf0c68d209c5
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-01-01 00:33:41 +0500

    Schema Added

---
+++ server/models/Doctors.model.js
@@ -0,0 +1,86 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const doctorSchema = mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  specialty: {
+    type: String,
+    required: true,
+  },
+  availableSlots: [
+    {
+      dayOfWeek: {
+        type: String,
+        required: true,
+      },
+      startTime: {
+        type: String,
+        required: true,
+      },
+      endTime: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  appointments: [
+    {
+      patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient",
+      },
+      appointmentDate: {
+        type: Date,
+        required: true,
+      },
+      appointmentTime: {
+        type: String,
+        required: true,
+      },
+      appointmentLocation: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+doctorSchema.pre("save", function (next) {
+  const doctor = this;
+
+  if (!doctor.isModified("password")) return next();
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(doctor.password, salt, function (err, hash) {
+      if (err) return next(err);
+      doctor.password = hash;
+      next();
+    });
+  });
+});
+doctorSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+module.exports = mongoose.Model("Doctor", doctorSchema);
---
+++ server/models/Patients.model.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const patientSchema = mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  phonenumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  appointments: [
+    {
+      doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor",
+      },
+      appointmentDate: {
+        type: Date,
+        required: true,
+      },
+      appointmentTime: {
+        type: String,
+        required: true,
+      },
+      appointmentLocation: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+patientSchema.pre("save", function (next) {
+  const patient = this;
+
+  if (!patient.isModified("password")) return next();
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(patient.password, salt, function (err, hash) {
+      if (err) return next(err);
+      patient.password = hash;
+      next();
+    });
+  });
+});
+patientSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+module.exports = mongoose.model("Patient", patientSchema);
