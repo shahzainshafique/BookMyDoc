0dcb4a94e3c14d45ef61890e6c62887033d685ec
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-02-27 20:46:00 +0500

    Playing with UI

--- client/src/Router/appRouter.jsx
+++ client/src/Router/appRouter.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterDoctor from "../Pages/auth/RegisterDoctor";
 import Landing from "../Pages/landing/landing";
+import SignUp from "../Pages/auth/SignUp";
 
 const AppRouter = () => {
   return (
@@ -8,6 +9,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/regDoc" element={<RegisterDoctor />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
