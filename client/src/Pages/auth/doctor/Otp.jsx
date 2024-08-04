de775f558e83429b5e0b837211c88287620158c1
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-08-04 21:26:33 +0500

    OTP frontend

--- client/src/Pages/auth/doctor/Otp.jsx
+++ client/src/Pages/auth/doctor/Otp.jsx
@@ -70,9 +70,9 @@ const Otp = () => {
     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
     <div className="max-w-md mx-auto text-center bg-white px-4 sm:px-8 py-10 rounded-xl shadow">
       <header className="mb-8">
-        <h1 className="text-2xl font-bold mb-1">Mobile Phone Verification</h1>
+        <h1 className="text-2xl font-bold mb-1">Email Verification</h1>
         <p className="text-[15px] text-slate-500">
-          Enter the 4-digit verification code that was sent to your phone number.
+          Enter the 4-digit verification code that was sent to your email.
         </p>
       </header>
       <form id="otp-form">
