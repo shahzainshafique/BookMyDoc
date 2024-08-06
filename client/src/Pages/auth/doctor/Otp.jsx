b9e9c209126da470334eb914bc809f155e346c0e
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-08-06 21:27:50 +0500

    OTP form button adjustment

--- client/src/Pages/auth/doctor/Otp.jsx
+++ client/src/Pages/auth/doctor/Otp.jsx
@@ -90,7 +90,7 @@ const Otp = () => {
         <div className="max-w-[260px] mx-auto mt-4">
           <button
             type="submit"
-            className="w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 transition-colors duration-150"
+            className="w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-primary-600 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 transition-colors duration-150"
           >
             Verify Account
           </button>
