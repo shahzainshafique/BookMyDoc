4244d9a3ca123fd01838544e21cb1da46e1b71fd
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-08-22 20:37:37 +0500

    Doctor Dashboard

---
+++ client/src/Components/dashboard/DoctorDashContent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DoctorDashContent = () => {
+  return <aside>DoctorDashContent</aside>;
+};
+
+export default DoctorDashContent;
--- client/src/Pages/dashboard/doctor/doctorDashboard.jsx
+++ client/src/Pages/dashboard/doctor/doctorDashboard.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import SideBar from "../../../Components/dashboard/SideBar";
+import DoctorDashContent from "../../../Components/dashboard/DoctorDashContent";
 
 const DoctorDashboard = () => {
-  return <SideBar />;
+  return (
+    <>
+      <SideBar />
+      <DoctorDashContent />
+    </>
+  );
 };
 
 export default DoctorDashboard;
