fe698817ad5dac9d49e1c81be84f72b76cde173c
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-08-28 20:51:58 +0500

    Patient Auth

--- client/src/Pages/auth/doctor/LoginDoc.jsx
+++ client/src/Pages/auth/doctor/LoginDoc.jsx
@@ -50,7 +50,7 @@ export default function LoginDoc() {
         <div className="w-full bg-white rounded-lg shadow light:border md:mt-0 sm:max-w-md xl:p-0 light:bg-gray-800 light:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl light:text-white">
-              Please Sign-in to proceed!
+              Please Sign-in to proceed as Doctor!
             </h1>
             <form
               className="space-y-4 md:space-y-6"
