7b492add5ef6e41d5e86954dfd2ac426d039fc3b
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-09-03 21:51:57 +0500

    Appointment backend

--- server/routes/patient.route.js
+++ server/routes/patient.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {createPatient, loginPatient, bookAppointment, cancelAppointment} = require('../controller/patient.controller');
+const { getTotalAppointments } = require('../controller/doctor.controller');
 
 const router = express.Router();
 
@@ -7,5 +8,6 @@ router.use('/signup',createPatient);
 router.use('/login',loginPatient);
 router.use('/book-appointment',bookAppointment);
 router.use('/cancel-appointment',cancelAppointment);
+router.use('/get-appointments',getTotalAppointments);
 
 module.exports= router;
