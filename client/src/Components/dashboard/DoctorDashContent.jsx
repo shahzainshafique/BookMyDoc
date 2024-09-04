d613dfa50c2a3c0813a0c450b4794a9f8f0ff99f
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-09-04 08:28:42 +0500

    Appointment backend

--- client/src/Components/dashboard/DoctorDashContent.jsx
+++ client/src/Components/dashboard/DoctorDashContent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DoctorDashContent = () => {
-  return <div className="flex-1 bg-white p-8">DoctorDashContent</div>;
+  return <div className="flex-1 p-8"><h1>Hello</h1>DoctorDashContent</div>;
 };
 
 export default DoctorDashContent;
--- client/src/Pages/dashboard/doctor/doctorDashboard.jsx
+++ client/src/Pages/dashboard/doctor/doctorDashboard.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import SideBar from "../../../Components/dashboard/SideBar";
 import DoctorDashContent from "../../../Components/dashboard/DoctorDashContent";
+import Header from "../../../Components/header/Header";
 
 const DoctorDashboard = () => {
-  return (
-    <div className="flex h-screen">
+  return (<>
+      <Header/>
+    <div className="h-screen flex">
       <SideBar />
       <DoctorDashContent />
     </div>
+    </>
   );
 };
 
