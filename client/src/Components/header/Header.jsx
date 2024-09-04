97a57e5af008c27aaebac4e56cef7abddd170915
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-09-04 20:05:34 +0500

    Dashboard

--- client/src/Components/header/Header.jsx
+++ client/src/Components/header/Header.jsx
@@ -15,12 +15,13 @@ function classNames(...classes) {
 
 export default function Header() {
   return (
-    <Disclosure as="nav" className="bg-blue-600">
+    <Disclosure as="nav" className="bg-blue-500">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
@@ -34,23 +35,28 @@ export default function Header() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-blue-900 text-white"
-                            : "text-gray-300 hover:bg-blue-900 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
+                <div className="home-left text-white">
+            <img
+              alt="image"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+              className="home-logo"
+              style={{width:'35px'}}
+            />
+            <h3 style={{fontWeight:"bold", fontSize:"20px", marginLeft:"-20px"}}>BookMyDoc</h3>
+            <nav className="home-links">
+              <a href="#features" className="home-link">
+                Features
+              </a>
+              <a href="#how-it-works" className="home-link01">
+                How it works
+              </a>
+              <span className="home-link02">Prices</span>
+              <a href="#schedule" className="home-link03">
+                Contact
+              </a>
+            </nav>
+          </div>
+          
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -131,27 +137,7 @@ export default function Header() {
               </div>
             </div>
           </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
+        
         </>
       )}
     </Disclosure>
