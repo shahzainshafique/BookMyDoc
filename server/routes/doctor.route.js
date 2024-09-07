94466977ef6bb36e2be1c30cad6d090b222e4e73
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-09-07 21:24:39 +0500

    Appointment Backend

--- server/routes/doctor.route.js
+++ server/routes/doctor.route.js
@@ -3,6 +3,7 @@ const {
   createDoctor,
   loginDoctor,
   getTotalAppointments,
+  fetchDoctorAppointments,
 } = require("../controller/doctor.controller");
 const { verifyToken } = require("../middleware/auth");
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.use("/signup", createDoctor);
 router.use("/login", loginDoctor);
 router.use("/get-appointments-count", verifyToken, getTotalAppointments);
+router.use("/appointments/:doctorId", verifyToken, fetchDoctorAppointments);
 
 module.exports = router;
