8b1031f2db3330fe4587775a16616ed19daf4a68
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-09-08 21:43:39 +0500

    Appointment Backend

--- client/src/Components/dashboard/doctor/DocCenterContent.jsx
+++ client/src/Components/dashboard/doctor/DocCenterContent.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import DocDashStats from './centerArea/DocDashStats'
+import React from "react";
+import DocDashStats from "./centerArea/DocDashStats";
+import DocTodayApp from "./centerArea/DocTodayApp";
 
 const DocCenterContent = () => {
   return (
-    <div className='flex flex-col bg-gray-100 rounded-3xl m-5 p-5 w-3/5'><DocDashStats/></div>
-  )
-}
+    <div className="flex flex-col bg-gray-100 rounded-3xl m-5 p-5 space-y-7 w-3/5">
+      <DocDashStats />
+      <DocTodayApp />
+    </div>
+  );
+};
 
-export default DocCenterContent
+export default DocCenterContent;
