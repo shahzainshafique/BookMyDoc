5da82e67fcdb3216168d52126f4d86aec15527e8
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-09-08 22:06:36 +0500

    Appointment Backend

---
+++ client/src/Components/common/Table.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Table = ({ columns, data }) => {
+  return (
+    <div className="overflow-x-auto">
+      <table className="min-w-full bg-white border border-gray-200">
+        <thead className="bg-gray-200">
+          <tr>
+            {columns.map((column) => (
+              <th
+                key={column.accessor}
+                className="py-2 px-4 border-b border-gray-200 text-left text-gray-600"
+              >
+                {column.Header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex} className="even:bg-gray-50">
+              {columns.map((column) => (
+                <td
+                  key={column.accessor}
+                  className="py-2 px-4 border-b border-gray-200 text-gray-800"
+                >
+                  {row[column.accessor]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
