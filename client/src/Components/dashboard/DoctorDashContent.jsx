c34c576b643017eebaa6bf8cbce13500d2343833
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-09-15 21:54:03 +0500

    Empty table handling

--- client/src/Components/dashboard/DoctorDashContent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DoctorDashContent = () => {
-  return <div className="flex-1 p-8"><h1>Hello</h1>DoctorDashContent</div>;
-};
-
-export default DoctorDashContent;
--- client/src/Pages/dashboard/doctor/doctorDashboard.jsx
+++ client/src/Pages/dashboard/doctor/doctorDashboard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import DocHeader from "../../../Components/dashboard/doctor/DocHeader";
 import DocSidebar from "../../../Components/dashboard/doctor/DocSidebar";
 import DocCenterContent from "../../../Components/dashboard/doctor/DocCenterContent";
