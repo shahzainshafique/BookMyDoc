63011bdd8d7bb60901995a2791bf8f73b9b87c0d
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-12-02 14:32:30 +0500

    Bug fixes

--- server/routes/doctor.route.js
+++ server/routes/doctor.route.js
@@ -15,8 +15,8 @@ const router = express.Router();
 router.use("/signup", createDoctor);
 router.use("/login", loginDoctor);
 router.use("/get-appointments-count", verifyToken, getTotalAppointments);
+router.use("/appointments/create",verifyToken,  createDocAppointment);
 router.use("/appointments/:doctorId", verifyToken, fetchDoctorAppointments);
-router.use("/appointments/create", verifyToken, createDocAppointment);
 router.use("/update-appointment", verifyToken, rescheduleAppointment);
 router.use("/cancel-appointment", verifyToken, cancelAppointment);
 
